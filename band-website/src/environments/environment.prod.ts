9d8caf2aa064cc149ddf252398207ca718dfda2d
Abe Kline <64223530+abramjkline@users.noreply.github.com>
2025-07-31 13:32:37 -0400

    deployment updates

---
+++ band-website/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    googleApiKey: {{GOOGLE_CALENDAR_API_KEY}}, // Your DEVELOPMENT API Key
+    // You can add other development-specific variables here
+  };
---
+++ band-website/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    googleApiKey: process.env.NG_APP_GOOGLE_CALENDAR_API_KEY, // Your DEVELOPMENT API Key
+    // You can add other development-specific variables here
+  };
